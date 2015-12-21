d0926e088bd46cf2216d7a1e2d6d69be9343c48a
Jeremy Keeshin <jkeesh@gmail.com>
2015-12-20 23:48:12 -0800

    add files

---
+++ README.md
@@ -0,0 +1,17 @@
+Simple bot to tweet out tsla quotes.
+
+Create an app here: https://apps.twitter.com/
+
+To use this, you must create a secrets.js file with your bot and keys of the format
+
+```
+module.exports = {
+    consumer_key:         'xxx'
+  , consumer_secret:      'xxx'
+  , access_token:         'xxx'
+  , access_token_secret:  'xxx'
+};
+```
+
+You can rename secrets-sample.js to secrets.js and add your twitter secrete keys to 
+make it work.
---
+++ main.js
@@ -0,0 +1,35 @@
+var request = require('request');
+var Twit = require('twit');
+var secrets = require('./secrets');
+
+var url = 'http://dev.markitondemand.com/Api/v2/Quote/json';
+var qs = {
+    symbol: 'SPY'
+};
+
+var T = new Twit(secrets);
+
+request({
+    url: url,
+    qs: qs
+
+}, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var result = JSON.parse(body);
+        console.log(result);
+        console.log('$' + result['LastPrice']);
+        console.log(result['MarketCap']);
+
+        var billion = 1000000000;
+        var marketCap = parseInt(result['MarketCap']) / billion;
+        marketCap = Math.floor(marketCap * 100) / 100;
+
+        var status = "TSLA: $" + result['LastPrice'] + " - Market Cap: $" + marketCap + "B " + new Date().toLocaleString();
+        console.log(status);
+
+        T.post('statuses/update', { status: status }, function(err, data, response) {
+          console.log(data);
+        });
+    }
+
+});
